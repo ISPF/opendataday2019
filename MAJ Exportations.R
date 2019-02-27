667401074ea141e9042939477b0770e97e9b45bd
Laurent PELLET <laurentp@ispf.pf>
2019-02-27 11:18:36 -1000

    Version pour demo

--- MAJ Exportations.R
+++ MAJ Exportations.R
@@ -1,13 +1,14 @@
-
 #############################
 # MISE A JOUR D'UNE RESSOURCE
 #############################
 
-i <- 7
-Tableaux[i,]
-dtFichier <- getTable(i)
-dtViaAPI <- DataGouv_GetResource(i)
+source("functions/Functions.R")
+DataGouv_ConfigureAPI(APIURL = "https://www.data.gouv.fr/api/1",
+                      ORGANIZATIONID = "56a29970c751df2a1cade714",
+                      APIKEY = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiNTZhMjk5MDdjNzUxZGYyYTFjYWRlNzEzIiwidGltZSI6MTU1MDE5MjE3Mi41MTM4MjN9.mYlcZ38Hx6GDFzSpHm-qewTIwMNMlZMncQqtVYJZm4o")
 
+i <- 7
+t(Tableaux[i,.(Titre, FileName,DatasetID, ResourceID)])
 DataGouv_UpdateResource(i)
-DataGouv_UpdateMetaData(i, "title", Tableaux[i,Titre])
-DataGouv_UpdateMetaData(i, "description", "Exportations 2018 de la Polynésie française")
+DataGouv_UpdateMetaData(i, "title", "Exportations en Polynésie française - 2019")
+DataGouv_UpdateMetaData(i, "description", "Demo Amphi CCISM")
---
+++ functions/autre code.R
@@ -0,0 +1,5 @@
+#Autre code
+
+#dtFichier <- getTable(i)
+#dtViaAPI <- DataGouv_GetResource(i)
+#DataGouv_UpdateMetaData(i, "title", Tableaux[i,Titre])
